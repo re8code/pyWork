7ed59157d251a4301d1da731f190ea7fab50ede4
re8code <won@re8code.com>
2025-07-26 23:21:45 +0900

    py2 Set, 7th code: 2-Dimension

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-import array as ar
-
-d = {}
-
-# Available as Dictionary Key
-# : integer, float, tuple, string, range, generator, map
-
-d[0] = 1                              # integer
-d[1.0] = 2                            # float
-# d[true] = 'error'                   # boolean     (X)
-# d[[1,2]] = 'error'                  # list        (X)
-d[2,] = 3                             # tuple
-d['str'] = 4                          # string
-# d[ar.array('i', [1, 2])] = 'error'  # array       (X)
-# d[{1:'a', 2:'b'}] = 'error'         # dictionary  (X)
-d[range(2)] = 5                       # range
-# d[{1,2}] = 'error'                  # set         (X)
-d[(i for i in range(2))] = 6          # generator
-d[map(int, [1,2])] = 7                # map
+s = set()
+s.add((1, 2, 3, 4, 5))
+s.add('build')
+s.add(range(9,0,-2))
 
 idx = 1
-for k in d.keys():
-    print(f'{idx}) key type: {type(k)}')
+for r in s:
+    print(f'{idx})', end=' ')
+    for c in range(len(r)): print(r[c], end=' ')
+    print(type(r))
     idx += 1
+s.clear()
+print('------------------------------')
+
+l = []
+l.append({1, 2, 3, 4, 5})
+l.append(set([2, 3, 4, 5, 6]))
+l.append({v for v in range(3, 8)})
+
+for r in range(3):
+    print(f'{r+1})', end=' ')
+    for c in l[r]: print(c, end=' ')
+    print(type(l[r]))
