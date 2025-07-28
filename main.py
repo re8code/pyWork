c892792d2e10b978f4771f6fdf09a4dea759a639
re8code <won@re8code.com>
2025-07-28 14:18:44 +0900

    py2 Lambda, 4th code: lambda & delegator

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-n, m = 70, 30
+def delegator(n, m, op):
+    if   op == 0: return (lambda a, b: a + b)(n, m)
+    elif op == 1: return (lambda a, b: a - b)(n, m)
+    elif op == 2: return (lambda a, b: a * b)(n, m)
+    elif op == 3: return (lambda a, b: a / b)(n, m)
+    else:         return (lambda a, b: a % b)(n, m)
 
-add = lambda a, b: a + b
-print(f'1) {n} + {m} = {add(n, m):4d}', type(add))
+a, b = 70, 30
 
-sub = lambda a, b: a - b
-print(f'2) {n} - {m} = {sub(n, m):4d}', type(sub))
-
-print(f'3) {n} x {m} = {(lambda a, b: a * b)(n, m):4d}')
-print(f'4) {n} / {m} = {(lambda a, b: a / b)(n, m):.2f}')
-
-def rem(a, b):
-    return a % b
-
-print(f'5) {n} % {m} = {rem(n, m):4d}', type(rem))
+print(f'1) {a} + {b} = {delegator(a, b, 0):4d}')
+print(f'2) {a} - {b} = {delegator(a, b, 1):4d}')
+print(f'3) {a} x {b} = {delegator(a, b, 2):4d}')
+print(f'4) {a} / {b} = {delegator(a, b, 3):.2f}')
+print(f'5) {a} % {b} = {delegator(a, b, 4):4d}')
