dc5a225f6cc92b6dee1424b565d502694ec1c6fe
re8code <won@re8code.com>
2025-07-30 09:48:35 +0900

    py2 15. 7th code: pattern-01

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ str = "010-1234-5678\n\
 thesecon@gmail.com\n\
 https://www.gildong.com/?p1=1412&p2=alskqwer\n\
 The quick brown fox jumps over the lazy dog.\n\
-abbcccdddd\
+abbcccdddd, hippy.\n\
+http://www.naver.com\
 "
 
-print(f'1) result      : {re.findall(r'^the', str)}')
-print(f'2) result      : {re.findall(r'^the', str, re.M)}')
-print(f'3) result      : {re.findall(r'^the', str, re.M|re.I)}')
-print(f'4) result      : {re.findall(r'com$', str, re.M)}')
-print(f'5) result      : {re.findall(r'fox|dog', str)}')
+print(f'1) result      : {re.findall(r'h..p', str)}')
+print(f'2) result      : {re.findall(r'https?', str)}')
+print(f'3) result      : {re.findall(r'dd*', str)}')
+print(f'4) result      : {re.findall(r'b+', str)}')
+
