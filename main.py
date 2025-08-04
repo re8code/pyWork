bc07f370332c7abded97f6ac030c6c867a3d01df
re8code <won@re8code.com>
2025-08-04 15:59:10 +0900

    py2 15. letter pattern-10

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import re
 
-str = "010-1234-5678\n\
-thesecon@gmail.com\n\
-https://www.gildong.com/?p1=1412&p2=alskqwer\n\
-The quick brown fox jumps over the lazy dog.\n\
-abbcccdddd, hippy.\n\
-http://www.naver.com"
+str = "my.domain.com\n\
+abcmy.domain.comxyz\n\
+www.gildong.com,\n\
+www.naver.com\n\
+www.naver-1.com\n\
+en.kakao.co.kr"
 
-result = re.findall(r'(bb|ww|\/\/)', str)
-print(f'1) result: {result}')
-
-result = re.findall(r'\b\w{2,}\.\w{3,}\.\w{2,}\b', str)
-print(f'2) result: {result}')
-
-result = re.findall(r'(\w{2,}\.\w{3,}\.\w{2,})', str)
-print(f'3) result: {result}')
+result = re.findall(r'\b\w{2,}\.\w{3,}\.\w{2,}\b', str);         print(f'1) result: {result}')
+result = re.findall(r'(\w{2,}\.\w{3,}\.\w{2,})',   str);         print(f'2) result: {result}')
+result = re.findall(r'\w{2,}\.\w{3,}\.\w{2,}',     str);         print(f'3) result: {result}')
