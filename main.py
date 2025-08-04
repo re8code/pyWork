8e91e47e41eaeac544d83a494e9b2c7333ac85dd
re8code <won@re8code.com>
2025-08-04 15:02:12 +0900

    py2 15. letter pattern-09(2)

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ thesecon@gmail.com\n\
 https://www.gildong.com/?p1=1412&p2=alskqwer\n\
 The quick brown fox jumps over the lazy dog.\n\
 abbcccdddd, hippy.\n\
-http://www.naver.com\
-"
+http://www.naver.com"
 
 result = re.findall(r'(bb|ww|\/\/)', str)
 print(f'1) result: {result}')
 
 result = re.findall(r'\b\w{2,}\.\w{3,}\.\w{2,}\b', str)
 print(f'2) result: {result}')
+
+result = re.findall(r'(\w{2,}\.\w{3,}\.\w{2,})', str)
+print(f'3) result: {result}')
