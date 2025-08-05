9b41b1e75c5044bc321f319ec540040490899b45
re8code <won@re8code.com>
2025-08-05 10:54:42 +0900

    py2 15. letter pattern-10(2)

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import re
 
 str = "my.domain.com\n\
-abcmy.domain.comxyz\n\
 www.gildong.com,\n\
 www.naver.com\n\
 www.naver-1.com\n\
 en.kakao.co.kr"
 
-result = re.findall(r'\b\w{2,}\.\w{3,}\.\w{2,}\b', str);         print(f'1) result: {result}')
-result = re.findall(r'(\w{2,}\.\w{3,}\.\w{2,})',   str);         print(f'2) result: {result}')
-result = re.findall(r'\w{2,}\.\w{3,}\.\w{2,}',     str);         print(f'3) result: {result}')
+result = re.findall(r'(\w{2,}\.\w{3,}\.\w{2,})', str)
+print(f'1) result: {result}')
+
+result = re.findall(r'\w{2,}\.\w{3,}\.\w{2,}', str)
+print(f'2) result: {result}')
+
+result = re.findall(r'([\w-]+(?:\.[\w-]+)+)', str)
+print(f'3) result: {result}')
