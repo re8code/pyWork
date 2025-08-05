bcbb689b487f01d47e1184a24c9c7e7e98b527c6
re8code <won@re8code.com>
2025-08-05 12:04:16 +0900

    py2 15. letter pattern-11

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import re
 
-str = "my.domain.com\n\
-www.gildong.com,\n\
-www.naver.com\n\
-www.naver-1.com\n\
-en.kakao.co.kr"
+str = "https://www.example.com과 http://company.net 웹사이트를 방문해 주세요. 문의사항은 010-1234-5678\
+ 또는 02-9876-5432로 전화 주시거나, contact@example.com이나 support@company.net으로 이메일을 보내주시면\
+ 됩니다."
 
-result = re.findall(r'(\w{2,}\.\w{3,}\.\w{2,})', str)
-print(f'1) result: {result}')
+result = re.findall(r'https?://[0-9a-zA-Z.]+', str)
+print(f'1) web URL  : {result}')
 
-result = re.findall(r'\w{2,}\.\w{3,}\.\w{2,}', str)
-print(f'2) result: {result}')
+result = re.findall(r'\d{2,4}-\d{4}-\d{4}', str)
+print(f'2) phone num: {result}')
 
-result = re.findall(r'([\w-]+(?:\.[\w-]+)+)', str)
-print(f'3) result: {result}')
+result = re.findall(r'[a-z]+@[a-z]+.[a-z]+', str)
+print(f'3) email add: {result}')
